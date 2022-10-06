bc993dcb2f75adddaa60e75d3df74feb0575bd9a
Trương Quốc Khánh <khanhtq@vmodev.com>
2022-10-06 17:20:37 +0700

    update project

--- src/schemas/catgegory/category.controller.ts
+++ src/schemas/catgegory/category.controller.ts
@@ -1,50 +1,10 @@
-import { Body, Controller, Get, HttpException, HttpStatus, Param, Patch, Post, UseGuards } from '@nestjs/common';
-import { ApiBadRequestResponse, ApiBearerAuth, ApiOkResponse, ApiTags } from '@nestjs/swagger';
-import { JwtAuthGuard } from '../auth/jwt-auth.guard';
-import { CATEGORY_SWAGGER_RESPONSE } from './category.constant';
+import { Controller } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { CategoryService } from './category.service';
-import { CreateCategoryDto } from './create-category.dto';
 
 @ApiTags('Category')
 @ApiBearerAuth()
 @Controller('categories')
 export class CategoryController {
   constructor(private readonly categroyService: CategoryService) {}
-
-  @ApiOkResponse(CATEGORY_SWAGGER_RESPONSE.CREATE_SUCCESS)
-  @ApiBadRequestResponse(CATEGORY_SWAGGER_RESPONSE.BAD_REQUEST_EXCEPTION)
-  @Post()
-  async create(@Body() body: CreateCategoryDto) {
-    const createdCategory = await this.categroyService.create(body);
-    if (!createdCategory) {
-      throw new HttpException('Create fail', HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-    return createdCategory;
-  }
-
-  @ApiOkResponse(CATEGORY_SWAGGER_RESPONSE.GET_LIST_SUCCESS)
-  @ApiBadRequestResponse(CATEGORY_SWAGGER_RESPONSE.BAD_REQUEST_EXCEPTION)
-  @Get()
-  async getAll() {
-    return this.categroyService.getAll();
-  }
-
-  @ApiOkResponse(CATEGORY_SWAGGER_RESPONSE.UPDATE_SUCCESS)
-  @ApiBadRequestResponse(CATEGORY_SWAGGER_RESPONSE.BAD_REQUEST_EXCEPTION)
-  @UseGuards(JwtAuthGuard)
-  @Patch(':id')
-  async update(@Param('id') id, @Body() body) {
-    const dataUpdate = {
-      ...body,
-      updatedBy: id,
-    };
-    return this.categroyService.update(id, dataUpdate);
-  }
-
-  @ApiOkResponse(CATEGORY_SWAGGER_RESPONSE.DELETE_SUCCESS)
-  @ApiBadRequestResponse(CATEGORY_SWAGGER_RESPONSE.BAD_REQUEST_EXCEPTION)
-  @Patch('delete/:id')
-  async delete(@Param('id') id) {
-    return this.categroyService.delete(id);
-  }
 }
--- src/schemas/catgegory/category.service.ts
+++ src/schemas/catgegory/category.service.ts
@@ -1,9 +1,8 @@
-import { ForbiddenException, HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Category, CategoryDocument } from './category.schema';
 import { Product } from '../product/product.schema';
 import { Model } from 'mongoose';
-import { CreateCategoryDto } from './create-category.dto';
 
 @Injectable()
 export class CategoryService {
@@ -12,26 +11,5 @@ export class CategoryService {
     private categoryModel: Model<CategoryDocument>,
     @InjectModel(Product.name)
     private productModel: Model<CategoryDocument>,
-  ) { }
-
-  async create(createdCategoryDto: CreateCategoryDto): Promise<CategoryDocument> {
-    const createdCategory = await this.categoryModel.create(createdCategoryDto);
-    return createdCategory;
-  }
-
-  async getAll(): Promise<CategoryDocument[]> {
-    return this.categoryModel.find().exec();
-  }
-
-  async update(id: string, dataUpdate: object) {
-    return this.categoryModel.updateOne({ _id: id }, dataUpdate);
-  }
-
-  async delete(id: string) {
-    const checkCategory = await this.productModel.findOne({ category: id });
-    if (checkCategory) {
-      throw new HttpException('The category contains products. Cannot delete', HttpStatus.BAD_REQUEST);
-    }
-    return this.categoryModel.updateOne({ _id: id }, { isDeleted: true });
-  }
+  ) {}
 }
